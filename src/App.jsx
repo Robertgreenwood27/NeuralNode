af7d617548e35458ddf21b82779a825658aad93c
RobertGreenwood27@gmail.com <RobertGreenwood27@gmail.com>
2024-10-10 07:39:43 -0600

    broke up styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,13 @@ import { auth } from './services/firebaseConfig';
 import { onAuthStateChanged } from 'firebase/auth';
 import FlowChart from './components/FlowChart/FlowChart';
 import Auth from './components/Auth/Auth';
-import './components/FlowChart/FlowChartBase.css';
-import './components/FlowChart/FlowChartComponents.css';
+import './components/FlowChart/styles/FlowChartBase.css';
+import './components/FlowChart/styles/GeneralStyles.css';
+import './components/FlowChart/styles/ChatInterfaceStyles.css';
+import './components/FlowChart/styles/FullScreenNodeStyles.css';
+import './components/FlowChart/styles/FABStyles.css';
+import './components/FlowChart/styles/BottomNavStyles.css';
+import './components/FlowChart/styles/ResponsiveStyles.css';
 import { FlowChartProvider } from './context/FlowChartProvider'; // Update this line
 
 function App() {
